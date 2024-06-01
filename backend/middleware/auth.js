f418dc8aadbcbb3ddc94a0eaf5aa2dd5c174314d
KelyanDev <therealmortem@gmail.com>
2024-06-01 10:59:23 +0200

    Auth changes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,21 +3,18 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        console.log(token);
         const decodedToken = jwt.verify(token, 'TOKEN_SECRET');
-        console.log(decodedToken);
         const userId = decodedToken.userId;
-        console.log(userId);
 
-        console.log(req.body);
         req.auth = { userId };
 
-        
-        if (req.body.userId && req.body.userId !== userId) {
+        /* === Verification of user's ID to access tickets === */
+        /* if (req.body.userId && req.body.userId !== userId) {
             throw 'Invalid user ID';
         } else {
             next();
-        };
+        }; */
+        next();
     } catch {
         console.info('here');
         res.status(401).json({
