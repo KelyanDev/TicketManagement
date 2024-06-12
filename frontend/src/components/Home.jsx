c4f6742cab7c99f65c7cad4dd675a652d7f1c809
KelyanDev <therealmortem@gmail.com>
2024-06-12 17:17:47 +0200

    Quick changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="home">
-            <img className="home-logo" src={`${process.env.PUBLIC_URL}/logo.png`} alt=""></img>
+            <img className="home-logo" src={`${process.env.PUBLIC_URL}/Logo.png`} alt=""></img>
             <h3 className="home-titre"> <b>A</b>uto<b>T</b>ech <b>S</b>upport</h3>
             <h4 className="home-text"> Notre application de gestion de tickets de support</h4>
         </div>
